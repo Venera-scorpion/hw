85aca1ad4da11c77ee6bb082cfa665ea0954c34a
venera <venaslanova@mail.ru>
2023-04-07 23:23:37 +0200

    min_or_max

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число А:");
+string text1 = Console.ReadLine();
+int number1 = Convert.ToInt32(text1);
+Console.Write("Введите число В:");
+string text2 = Console.ReadLine();
+int number2 = Convert.ToInt32(text2);
+if(number1 > number2)
+{
+    Console.WriteLine("Число А большее");
+    Console.WriteLine("Число В меньшее");
+}
+    else 
+    {
+     Console.WriteLine("Число А меньшее");
+    Console.WriteLine("Число В большее");
+    }
